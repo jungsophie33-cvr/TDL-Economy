0f1fcfc49dc3702cadd6cdf052715d476d96b9ae
jungsophie33-cvr <jung.sophie33@gmail.com>
2025-10-22 19:34:23 +0200

    Add console log for successful script load

--- eco-forum.js
+++ eco-forum.js
@@ -3,6 +3,7 @@
 // Objectif : système d’économie complet via JSONBin + intégration ModernBB
 // Héberger ce fichier sur Archive-Host, GitHub Gist, etc.
 // Puis charger sur ton forum via : $.getScript("https://tonurl/eco-forum.js");
+console.log("[EcoV2] Fichier eco-forum.js bien chargé !");
 
 const BIN_ID = "68f89576d0ea881f40b2b27b";
 const API_KEY = "$2a$10$yVl9vTE.d/B4Hbmu8n6pyeHDM9PgPVHCBryetKJ3.wLHr7wa6ivyq";
