3abf4a04a67b2ad87af1e4c583f7a122736f8c8a
jungsophie33-cvr <jung.sophie33@gmail.com>
2026-08-07 09:14:32 +0200

    Update and rename fiche-utils-v4.js to fiche-utils-v4-1.js

--- scripts-validation-fiche/fiche-utils-v4-1.js
+++ scripts-validation-fiche/fiche-utils-v4-1.js
@@ -177,7 +177,7 @@
 <d>Faceclaim</d> <span>${esc(d.faceclaim)}</span>
 <d>Groupe</d> <span>${esc(d.groupe)}</span></div><div class="sj-formcol"><f4>Personnage & bottins</f4><d>Bande hors-la-loi</d> ${bande}
 <d>Métier</d> <span>${FI.metierResume(d)}</span>
-<d>Habitation</d> <span>${esc(d.lieu_habitation)}</span> <span>${esc(d.numero)}</span> <span>${esc(d.type_logement)}</span></div>
+<d>Habitation</d> ${FI.habitationResume(d)}
 </div></div>`;
   };
 
